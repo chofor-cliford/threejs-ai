f4f9704998df4c0f13f57f6e4a91a5b282c1bcdf
Better Futures <betterfuture032@gmail.com>
2023-05-29 07:05:29 -0600

    first commit

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,43 @@
+import CustomButton from "./CustomButton";
+
+// eslint-disable-next-line react/prop-types
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        className="aipicker-textarea"
+        placeholder="Ask AI..."
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        rows={5}
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AIPicker;
